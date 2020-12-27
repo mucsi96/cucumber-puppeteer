2c7c16943a5cae5d8ca85a59082806ee3323db10
Igor Bari <mucsi96@gmail.com>
2020-12-27 08:22:22 +0100

    Improve data snapshots

--- packages/cucumber-puppeteer/src/dataSnapshot.ts
+++ packages/cucumber-puppeteer/src/dataSnapshot.ts
@@ -3,11 +3,12 @@ import { page } from ".";
 
 type NodeData =
   | string
+  | null
   | undefined
   | {
       name?: string;
-      value?: NodeData;
-      children?: NodeData[];
+      value?: string | null;
+      children?: NodeData;
     }
   | NodeData[];
 
@@ -17,47 +18,49 @@ function getDataSnapshotPageFunction(elements: Element[]) {
   }
 
   function getNodeData(node: Node): NodeData {
-    if (node.nodeType === Node.TEXT_NODE) {
-      return node.textContent?.trim();
-    }
-
     const children = Array.prototype.map
       .call(node.childNodes, getNodeData)
       .filter(Boolean) as NodeData[];
     const name = getNodeName(node as Element);
 
+    if (!name) {
+      if (!children.length) {
+        return null;
+      }
+
+      if (children.length === 1) {
+        return children[0];
+      }
+
+      return children;
+    }
+
     if (!children.length) {
-      return;
+      return { name, value: node.textContent };
     }
 
     if (children.length === 1) {
-      if (name) {
-        return { name, value: children[0] };
-      }
-
-      if (typeof children[0] === "string") {
-        return;
-      }
-
-      return children[0];
-    }
-
-    if (!name) {
-      return children;
+      return { name, children: children[0] };
     }
 
     return { name, children };
   }
 
-  const data = elements
-    .map(getNodeData)
-    .map((elementData) =>
-      typeof elementData === "object" &&
-      elementData !== null &&
-      "children" in elementData
-        ? elementData.children
-        : elementData
-    );
+  const data = elements.map(getNodeData).map((elementData) => {
+    if (
+      typeof elementData !== "object" ||
+      elementData === null ||
+      Array.isArray(elementData)
+    ) {
+      return elementData;
+    }
+
+    if (elementData.children) {
+      return elementData.children;
+    }
+
+    return elementData.value;
+  });
 
   return JSON.stringify(data.length > 1 ? data : data[0]);
 }
